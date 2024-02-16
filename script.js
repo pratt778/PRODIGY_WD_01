fd349440688b3111424b6f83b195cd99cedd3bf9
Pratham Sharma <pratham016914934@gmail.com>
2024-02-16 20:26:45 +0545

    added darkmode

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 let container = document.querySelector('.hamburger-cont');
 let navcont = document.querySelector('.navbar-cont');
 let header = document.querySelector('.header-cont');
+let light = document.querySelector('.light-btn');
+let dark = document.querySelector('.dark-btn');
 opennav = ()=>{
     container.classList.toggle('active');
     navcont.classList.toggle('active-bar')
@@ -16,4 +18,11 @@ window.onscroll = function(){
     else{
         header.classList.remove('headcol');
     }
+}
+
+modechange=()=>{
+    light.classList.toggle('active-btn');
+    dark.classList.toggle('active-btn');
+    
+    document.body.classList.toggle('dark-mode');
 }
